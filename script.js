9b4e6c42b4b14086827f2298760063b6df85f386
Matsushita <m.d.h940810@gmail.com>
2024-06-20 23:17:58 +0900

    for web application change commit

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 $(function () {
-
-  //ページ内スクロール
+  // ページ内スクロール
   var navHeight = $(".header").outerHeight();
 
   $('a[href^="#"]').on("click", function () {
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? "html" : href);
     var position = target.offset().top - navHeight;
-    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    $("html, body").animate({ scrollTop: position }, 300, "swing");
     return false;
   });
 
-  //ページトップ
+  // ページトップ
   $("#js-page-top").on("click", function () {
-    $("body,html").animate({ scrollTop: 0, }, 300);
+    $("body,html").animate({ scrollTop: 0 }, 300);
     return false;
   });
 
+  // スクロールアニメーション
+  $(window).on("scroll", function () {
+    $(".fadein").each(function () {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 100) {
+        $(this).addClass("scrollin");
+      }
+    });
+  });
 });
